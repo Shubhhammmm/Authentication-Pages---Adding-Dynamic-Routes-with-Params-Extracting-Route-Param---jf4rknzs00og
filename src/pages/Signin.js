b95edd635aadca5af312493b7ce6a13cb1b5a5a2
Shubham Shrivastava <128039947+Shubhhammmm@users.noreply.github.com>
2023-06-14 12:32:56 +0530

    Update Signin.js

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router';
+import { signin } from '../api/auth';
+
 
 
 export const Signin = ()=>{
+    const [err,setErr]=React.useState('')
+    const navigate=useNavigate()
+    const emailref=useRef()
+    const passRef =useRef()
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        const email = emailref.current.value
+        const password = passRef.current.value
+        signin({email,password}).then(()=>{
+            localStorage.setItem('isLoggedIn',true)
+            navigate('/home')
+        }).catch(()=>setErr("Wrong email or password"))
+    }
     return(
         <div id="signin-page">
-            <h1>Sign in page</h1>
+            <h1>Sign in page</h1>  
+
+              <form onSubmit={handleSubmit}>
+                <input type="email" ref={emailref} placeholder='email' required/>
+                <br/>
+                <input type="password" ref={passRef} placeholder='password' required/>
+                <br/>
+                <button type="submit">Sign in</button>
+            </form>
+            <div className='error-txt'>{err}</div>
+
 
         </div>
     )
